0c96274cefd37fdab01533aa41723ea6e1fb438f
Platon T <platon@meloboss.ru>
2025-09-23 19:48:59 +0300

    добавление фикстуры

--- selenium_and_pytest.py
+++ selenium_and_pytest.py
@@ -9,28 +9,32 @@ fake = Faker()
 fake_login = fake.user_name()
 fake_password = fake.password()
 
+@pytest.fixture()
+def page():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get('https://store.steampowered.com/')
+    return driver
+
+
 @pytest.mark.parametrize(
     'user',
     [
         (fake_login, fake_password)
     ]
 )
-def test_error_message_for_auth(user):
+def test_error_message_for_auth(user, page):
     login, password = user
 
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.get('https://store.steampowered.com/')
-    driver.find_element(By.CLASS_NAME, 'global_action_link').click()
-    driver.find_element(By.XPATH, '//input[@type="text" and not(@id)]').send_keys(login)
-    driver.find_element(By.XPATH, '//input[@type="password" and not(@id)]').send_keys(password)
-    driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+    page.find_element(By.CLASS_NAME, 'global_action_link').click()
+    page.find_element(By.XPATH, '//input[@type="text" and not(@id)]').send_keys(login)
+    page.find_element(By.XPATH, '//input[@type="password" and not(@id)]').send_keys(password)
+    page.find_element(By.XPATH, '//button[@type="submit"]').click()
 
-    error_message = driver.find_element(
+    error_message = page.find_element(
         By.XPATH,
         '//*[contains(text(), "Пожалуйста, проверьте свой пароль")]'
     ).text
 
     assert "Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова." == error_message
-
 
