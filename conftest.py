b087659277e700834b41ab38de9c04c07095ac14
Platon T <platon@meloboss.ru>
2025-09-24 14:11:37 +0300

    Добавление конфтеста

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+URL = 'https://store.steampowered.com/'
+TIMEOUT_ELEMENT = 10
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(TIMEOUT_ELEMENT)
+    driver.get(URL)
+    return driver
